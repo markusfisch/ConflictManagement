b5fc23f8f39f1e99b1f0b60cf008812a1968fdfd
Markus Fisch <mf@markusfisch.de>
2019-08-18 18:16:26 +0200

    Remove historical event propagation aliases

--- src.js
+++ src.js
@@ -515,8 +515,6 @@ function setPointer(event, down) {
 
 	event.preventDefault()
 	event.stopPropagation()
-	event.cancelBubble = true
-	event.returnValue = false
 }
 
 function pointerCancel(event) {
