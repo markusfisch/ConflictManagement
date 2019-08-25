bce54ed02a94f399a53bffec5774b8c4f95dc87f
Markus Fisch <mf@markusfisch.de>
2019-08-25 19:43:40 +0200

    Clamp shadop map to edge
    Or there will be phantom shadows in the distance,
    of course.

--- src.js
+++ src.js
@@ -827,8 +827,8 @@ function createShadowBuffer() {
 	gl.bindTexture(gl.TEXTURE_2D, shadowDepthTexture)
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, shadowDepthTextureSize,
 		shadowDepthTextureSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, null)
 
