e1a5790de9c27fef1e004360ee83ad3a6f72da86
Markus Fisch <mf@markusfisch.de>
2019-11-03 11:17:52 +0100

    Remove clamping textures to edge
    Fall back to use the default value of REPEAT and save
    two lines.
    
    I just CLAMP_TO_EDGE because it makes the edges of the
    shadow map visible.

--- src.js
+++ src.js
@@ -1964,8 +1964,6 @@ function createTexture() {
 	gl.bindTexture(gl.TEXTURE_2D, texture)
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 	return texture
 }
 
